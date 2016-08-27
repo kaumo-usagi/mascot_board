09d39c25163edc59ac489ad724b9bebe0ea28d57
take-sei <sei06k14@gmail.com>
2016-08-27 19:06:48 +0900

    made a build env.

--- src/scripts/js/index.js
+++
@@ -1,2 +0,0 @@
-Parse Error: Line 22: Illegal export declaration
-for file /Users/Shotaro/Documents/Develop/hackthon/mascot_board/src/index.jsx
--- src/tag/App.js
+++
@@ -1,2 +0,0 @@
-Parse Error: Line 1: Illegal import declaration
-for file /Users/Shotaro/Documents/Develop/hackthon/mascot_board/src/tag/App.jsx
