09a294f226b433f199c5c267a5be765fce5ae71e
taka-sho <takasho530000@gmail.com>
2016-08-27 23:52:32 +0900

    First using material-ui

--- containers/App.jsx
+++ containers/App.jsx
@@ -1,14 +1,16 @@
 import { connect } from "react-redux";
 // import your original components from components directory
 // ex) import WhiteBoard from "../components/WhiteBoard";
+import SideBar from "../components/side_bar.jsx"
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
 	  return (
-					<div>
-					  //write your component
-					</div>
-					 );
+		<div>
+      <SideBar />
+		</div>
+	 );
 	}
 
 }
