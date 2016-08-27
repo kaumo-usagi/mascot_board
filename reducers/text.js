47fc1948928511b3b9179d9d1e5245252e98fbc9
keiohack <develop@tk2-229-24449.vs.sakura.ne.jp>
2016-08-28 04:24:44 +0900

    Add reducers methods

--- reducers/text.js
+++ reducers/text.js
@@ -1,3 +1,12 @@
-export default function(state = [], action) {
-  return state;
+import {ADD_TXT, DELETE_TXT, UPDATE_TXT} from "../src/constant"
+
+export default function img(state=[], action) {
+  switch (action.type) {
+    case ADD_TXT :
+      return state.concat({txt: action.txt, position: action.position, id: action.id});
+    case DELETE_TXT :
+      return state.filter( (e)=>{ return (e.id != action.id) } );
+    case UPDATE_TXT :
+      return state.map( (e)=>{ if(e.id === action.id){ return { txt: action.txt, position: action.position } } else{ return e }} );
+  }
 }
