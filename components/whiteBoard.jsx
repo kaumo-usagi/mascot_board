46dc10c9fbbe4efb98f018f68f510ab1caa618fc
taka-sho <takasho530000@gmail.com>
2016-08-28 00:39:54 +0900

    Add whiteBoard

---
+++ components/whiteBoard.jsx
@@ -0,0 +1,31 @@
+// use this directory as a components folder
+import React, { PropTypes, Component } from "react";
+import Paper from 'material-ui/Paper';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+const style = {
+  height: 1000,
+  width: "calc(100% - 40px)",
+  margin: 20,
+  textAlign: 'center',
+  display: 'inline-block',
+};
+
+class WhiteBoard extends Component {
+  getChildContext() {
+    return {muiTheme: getMuiTheme(baseTheme)};
+  }
+  render() {
+    return (
+      <div>
+        <Paper style={style} zDepth={1} />
+      </div>
+    )
+  }
+}
+WhiteBoard.childContextTypes = {
+  muiTheme: PropTypes.object.isRequired
+}
+
+export default WhiteBoard;
