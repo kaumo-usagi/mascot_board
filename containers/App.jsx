6aeb6bc31f9670bbb296f5a76547e9e3d95ac188
take-sei <sei06k14@gmail.com>
2016-08-27 19:21:30 +0900

    fix commit bug

---
+++ containers/App.jsx
@@ -0,0 +1,16 @@
+import { connect } from "react-redux";
+// import your original components from components directory
+// ex) import WhiteBoard from "../components/WhiteBoard";
+
+class App extends Component {
+  render() {
+	  return (
+					<div>
+					  //write your component
+					</div>
+					 );
+	}
+
+}
+
+export default App;
