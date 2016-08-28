4a5189668df1b496936ce19b7f458d35e9232f31
keiohack <develop@tk2-229-24449.vs.sakura.ne.jp>
2016-08-28 12:05:08 +0900

    Rebase feature/stamp to feature/logic

--- components/WhiteBoard.jsx
+++ components/WhiteBoard.jsx
@@ -1,11 +1,11 @@
 import { Stage, Layer, Text, Circle } from "react-konva";
-import React from "react";
+import React, {Component} from "react";
 import { connect } from "react-redux";
 import Paper from 'material-ui/Paper';
 
 import {
   ws,
-  MOVE_CURSOR
+    MOVE_CURSOR
 } from "../src/constant";
 
 const width = window.innerWidth - (240 + 40 + 40);
@@ -32,21 +32,6 @@ class WhiteBoard extends Component {
     this.state = { lastUpdatedAt: new Date().getTime() };
   }
 
-  getChildContext() {
-    return {muiTheme: getMuiTheme(baseTheme)};
-  }
-
-  textStyle() {
-    return {
-      top:  100,
-      left: 0,
-      width: 100,
-      height: 20,
-      lineHeight: 20,
-      fontSize: 12
-    };
-  }
-
   onMouseMove(e) {
     const now = new Date().getTime();
     if (now - this.state.lastUpdatedAt > 30) {
@@ -66,18 +51,18 @@ class WhiteBoard extends Component {
       }
     });
     return (
-      <div id="whiteBoard">
+        <div id="whiteBoard">
         <Paper style={style} zDepth={1} onMouseMove={this.onMouseMove.bind(this)} >
-          <Stage width={width} height={width}>
-            <Layer>
-              { circles }
-              <Text fill="#666666" x={100} y={300} text={"Test text"} fontSize={30} />
-            </Layer>
-          </Stage>
+        <Stage width={width} height={width}>
+        <Layer>
+        { circles }
+        <Text fill="#666666" x={100} y={300} text={"Test text"} fontSize={30} />
+        </Layer>
+        </Stage>
         </Paper>
-      </div>
-);
-
+        </div>
+        );
+  }}
 const mapStateToProps = state => ({
   users: state.users
 });
@@ -86,8 +71,8 @@ const mapDispatchToProps = dispatch => ({
 });
 
 const WhiteBoardContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(WhiteBoard);
+    mapStateToProps,
+    mapDispatchToProps
+    )(WhiteBoard);
 
 export default WhiteBoardContainer;
