ec144019d5188f9375cc2677b14fcd2729192b05
izumin5210 <masayuki@izumin.info>
2016-08-28 13:35:40 +0900

    Rename: reducers/img -> reducers/images

--- reducers/index.js
+++ reducers/index.js
@@ -1,12 +1,12 @@
-import { combineReducers } from "redux"
-import texts from "./texts"
-import img from "./img"
-import free_hand from "./free_hand"
-import users from "./users"
+import { combineReducers } from "redux";
+import texts from "./texts";
+import images from "./images";
+import free_hand from "./free_hand";
+import users from "./users";
 
 export default combineReducers({
   texts,
-  img,
+  images,
   free_hand,
   users
-})
+});
